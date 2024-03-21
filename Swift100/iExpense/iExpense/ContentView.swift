ddea702d3ec085d726b59e5dd35c6408caa9c12f
liqiang <liqiang@orvibo.com>
2024-03-21 11:30:34 +0800

    Day37、38-iExpense,第二、三部分

--- Swift100/iExpense/iExpense/ContentView.swift
+++ Swift100/iExpense/iExpense/ContentView.swift
@@ -7,15 +7,112 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem:Identifiable,Codable {
+    var id = UUID()
+    let name:String
+    let type:String
+    let amount:Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodeItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodeItems
+                return
+            }
+        }
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                //由于我们的费用项目现在保证是唯一可识别的，我们不再需要告诉ForEach用于标识符的属性——它知道将有一个id属性，并且它将是唯一的，因为这是Identifiable协议的重点。
+                ForEach(expenses.items) {item in
+                    HStack {
+                        VStack(alignment:.leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount,format:.currency(code: "USD"))
+                            .foregroundStyle(item.amount < 10 ? .blue : item.amount < 100 ? .red : .yellow)
+                    }
+                }
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense",systemImage: "plus") {
+
+                    showingAddExpense.toggle()
+                }
+            }
+        }
+        .sheet(isPresented:$showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItem(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+
+struct AddView : View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    var expenses:Expenses
+    let types = ["Business","Personal"]
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name",text: $name)
+                Picker("Type",selection: $type) {
+                    ForEach(types,id: \.self)  {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount",value: $amount,format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let expense = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(expense)
+                    expenses.items.sort {
+                        if $0.type == $1.type  {
+                          return  $0.amount > $1.amount
+                        }
+                        return  $0.type > $1.type
+                    }
+                    dismiss()
+                }
+                
+            }
+        }
     }
 }
 
