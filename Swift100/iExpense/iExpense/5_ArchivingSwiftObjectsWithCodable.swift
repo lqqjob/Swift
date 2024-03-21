0f1baea73b5bee756f2b4778576f940ff164cf2d
liqiang <liqiang@orvibo.com>
2024-03-21 09:59:15 +0800

    Day36-iExpense,第一部分

--- Swift100/iExpense/iExpense/5_ArchivingSwiftObjectsWithCodable.swift
+++ Swift100/iExpense/iExpense/5_ArchivingSwiftObjectsWithCodable.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 
-struct People {
+struct People: Codable {
     let userName:String
     let lastName:String
 }
 
 struct ArchivingSwiftObjectsWithCodable: View {
+    @State private var user = People(userName: "Taylor", lastName: "Swift")
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data,forKey: "UserData")//这直接访问UserDefaults而不是通过@AppStorage，因为@AppStorage属性包装器在这里不起作用。
+            }
+        }
     }
 }
 
