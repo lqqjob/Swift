6882512722fa55d9cc2a757608a8c1ebf16ec9f6
liqiang <liqiang@orvibo.com>
2024-04-04 22:45:14 +0800

    Day55-Bookworm,第三部分

--- Swift100/Bookworm/Bookworm/ContentView.swift
+++ Swift100/Bookworm/Bookworm/ContentView.swift
@@ -10,23 +10,35 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query var books:[Book]
+    @Query(sort: \Book.title) var book1:[Book]
+    @Query(sort:\Book.rating,order: .reverse) var book2:[Book]
+    @Query(sort:[SortDescriptor(\Book.title)]) var book3:[Book]
+    @Query(sort:[SortDescriptor(\Book.title,order: .reverse)]) var book4:[Book]
+    @Query(sort:[
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var book5:[Book]
+
     @State private var showingAddScreen = false
     var body: some View {
         NavigationStack {
             List {
                 ForEach(books) {book in
-                    HStack {
-                        EmojiRatingView(rating: book.rating)
-                            .font(.largeTitle)
-                        VStack(alignment: .leading, content: {
-                            Text(book.title)
-                                .font(.headline)
-                            Text(book.author)
-                                .foregroundStyle(.secondary)
-                        })
+                    NavigationLink(value:book) {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            VStack(alignment: .leading, content: {
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            })
+                        }
                     }
                     
                 }
+                .onDelete(perform: deleteBooks)
             }
                 .navigationTitle("Bookworm")
                 .toolbar {
@@ -36,10 +48,24 @@ struct ContentView: View {
                         }
                         
                     }
+                    ToolbarItem(placement: .topBarLeading) {
+                        EditButton()
+                    }
                 }
                 .sheet(isPresented: $showingAddScreen, content: {
                     AddBookView()
                 })
+                .navigationDestination(for: Book.self) {book in
+                    DetailView(book: book)
+                    
+                }
+        }
+    }
+    
+    func deleteBooks(at offsets:IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            modelContext.delete(book)
         }
     }
 }
