ce184b2e52f31fa2d065f83719624cc8079371ca
liqiang <liqiang@orvibo.com>
2024-05-11 23:02:02 +0800

    Day83-HotProspects,第五部分

--- Swift100/HotProspects/HotProspects/ProspectsView.swift
+++ Swift100/HotProspects/HotProspects/ProspectsView.swift
@@ -24,22 +24,51 @@ struct ProspectsView: View {
             "Uncontacted people"
         }
     }
+    @State private var selectedProspects = Set<Prospect>()
     var body: some View {
         NavigationStack {
-            List(prospects) {prospect in
+            List(prospects,selection: $selectedProspects) {prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted",systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    }else {
+                        Button("Mark Contacted",systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                   
+                }
+                .tag(prospect)
                 
             }
                 .navigationTitle(title)
                 .toolbar {
-                    Button("Scan",systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
-                        modelContext.insert(prospect)
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Scan", systemImage: "qrcode.viewfinder") {
+//                            isShowingScanner = true
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        EditButton()
+                            
+                    }
+                    if selectedProspects.isEmpty == false {
+                        ToolbarItem(placement: .bottomBar) {
+                            Button("Delete Selected", action: delete)
+                        }
                     }
                 }
         }
@@ -52,6 +81,12 @@ struct ProspectsView: View {
             _prospects = Query(filter:#Predicate{
                 $0.isContacted == showContactedOnly
             },sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
