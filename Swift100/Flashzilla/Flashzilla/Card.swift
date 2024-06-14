abf58482abd02a46b152404581c190ef52065783
liqiang <liqiang@orvibo.com>
2024-06-14 16:34:42 +0800

    Day88-Flashzilla,第三部分

---
+++ Swift100/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by liqiang on 2024/6/14.
+//
+
+import Foundation
+
+struct Card {
+    var promot:String
+    var answer:String
+    static let example = Card(promot: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
