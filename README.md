7531c912e9070d26eb37c06484647734947e1716
liqiang <liqiang@orvibo.com>
2024-12-31 09:59:11 +0800

    iAppStore 项目

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 学习网站
 1. https://www.avanderlee.com/swift
 2. https://www.hackingwithswift.com/100/swiftui
+3. https://github.com/37MobileTeam/iAppStore-SwiftUI
---
+++ iAppStore/iAppStore/iAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAppStoreApp.swift
+//  iAppStore
+//
+//  Created by liqiang on 2024/12/31.
+//
+
+import SwiftUI
+
+@main
+struct iAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
