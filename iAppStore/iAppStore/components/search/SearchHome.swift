28c7317d0e913900ceaf78b632d93a5100221f40
mlqq <lqqjob@163.com>
2025-01-22 23:06:58 +0800

    搜索页面

--- iAppStore/iAppStore/components/search/SearchHome.swift
+++ iAppStore/iAppStore/components/search/SearchHome.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct SearchHome: View {
+    @State private var isSearching = false
+    @State private var searchText = ""
+    @AppStorage("kSearchRegionName") private var regionName:String = "中国"
+    @State private var filterViewIsExpanded = false
+    @StateObject private var appModel = AppDetailModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
