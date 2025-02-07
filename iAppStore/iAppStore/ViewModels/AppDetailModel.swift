97978d5bd06711efe3b7e5dba9613e62282bd287
liqiang <liqiang@orvibo.com>
2025-02-07 16:31:54 +0800

    搜素app

--- iAppStore/iAppStore/ViewModels/AppDetailModel.swift
+++ iAppStore/iAppStore/ViewModels/AppDetailModel.swift
@@ -14,6 +14,64 @@ class AppDetailModel:ObservableObject {
     
     func searchAppData(_ appId:String?,_ keyWord:String?,_ regionName:String) {
         let regionId = TSMGConstants.regionTypeListIds[regionName] ?? "cn"
+        var endPoint:APIService.EndPoint = APIService.EndPoint.lookupApp(appid: "", country: "")
+        if let appId = appId {
+            endPoint = APIService.EndPoint.lookupApp(appid: appId, country: regionId)
+        }
+        if let word = keyWord,let encodeword = word.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            endPoint = APIService.EndPoint.searchApp(word: encodeword, country: regionId, limit: 200)
+        }
         
+        isLoading = true
+        
+        APIService.shared.POST(endpoint: endPoint, params: nil) {(result: Result<AppDetailM, APIService.APIError>) in
+            self.isLoading = false
+            switch result {
+            case let .success(response):
+                self.results = response.results
+                if appId != nil {
+                    self.app = response.results.first
+                }
+                if let word = keyWord {
+                    self.lookupBundleId(word: word, regionId: regionId)
+                }
+            case .failure(_):
+                break
+            }
+            
+        }
+    }
+    
+    func lookupBundleId(word:String,regionId:String) {
+        guard let bundleId = word.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        let endPoint = APIService.EndPoint.lookupBundleId(appid: bundleId, country: regionId)
+        APIService.shared.POST(endpoint: endPoint, params: nil) { (result: Result<AppDetailM, APIService.APIError>)  in
+            switch result {
+            case .success(let success):
+                if let app = success.results.first {
+                    self.results.insert(app, at: 0)
+                }
+            case .failure(_):
+                break
+            }
+        }
+    }
+    
+    func lookupAppId(_ appId:String,_ regionName:String) {
+        let regionId = TSMGConstants.regionTypeListIds[regionName] ?? "cn"
+        let endPoint = APIService.EndPoint.lookupApp(appid: appId, country: regionId)
+        APIService.shared.POST(endpoint: endPoint, params: nil) {(result: Result<AppDetailM, APIService.APIError>) in
+            switch result {
+            case let .success(response):
+                if let app = response.results.first {
+                    self.results.append(app)
+                }
+            case .failure(_):
+                break
+            }
+        }
     }
 }
