1444df4968229c74850b5545855aaeb0664d354c
mlqq <lqqjob@163.com>
2025-03-10 23:06:14 +0800

    排行页面

---
+++ iAppStore/iAppStore/ViewModels/AppRankModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppRankModel.swift
+//  iAppStore
+//
+//  Created by Mlqq on 2025/3/10.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+class AppRankModel:ObservableObject {
+    @Published var rankTitle:String = "排行榜"
+    @Published var rankUpdated:String = ""
+    @Published var results:[AppRank] = []
+    
+    @Published var alertMsg:String = ""
+    @Published var isShowAlert:Bool = false
+    private var cancelable = Set<AnyCancellable>()
+    
+    
+}
--- iAppStore/iAppStore/components/rankLists/RankHome.swift
+++ iAppStore/iAppStore/components/rankLists/RankHome.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct RankHome: View {
+    @AppStorage("kRankTypeName") private var rankName:String = "热门免费榜"
+    @AppStorage("kRankCategoryName") private var categoryName:String = "所有 App"
+    @AppStorage("kRankReginName") private var regionName:String = "中国"
+    @State var isShowAlert = false
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
