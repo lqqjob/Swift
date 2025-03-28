2db4b7c872ad1af686080ba9e88f2f3dbf20b1f1
liqiang <liqiang@orvibo.com>
2025-03-28 17:26:25 +0800

    首页筛选框

--- iAppStore/iAppStore/components/rankLists/RankSortView.swift
+++ iAppStore/iAppStore/components/rankLists/RankSortView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct RankSortView: View {
+    
+    public enum RankSortType:Int {
+        case noneType
+        case rankType
+        case categoryType
+        case regionType
+    }
+    
+    @Binding var rankName:String
+    @Binding var categoryName:String
+    @Binding var regionName:String
+    
+    @State private var sortViewIsExpanded:Bool = false
+    @State private var isViewAppear:Bool = false
+    
+    var action:((_ rankName:String,_ categoryName:String,_ regionName:String) -> Void)?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    RankSortView()
+    RankSortView(rankName: .constant("1"), categoryName:  .constant("1"), regionName:  .constant("1")) { rankName, categoryName, regionName in
+        print("")
+    }
 }
