c2e954109a3d4ce7f3b12f2450ff4094099cf519
liqiang <liqiang@orvibo.com>
2025-04-16 15:52:34 +0800

    SwiftUI进阶学习

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by liqiang on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
