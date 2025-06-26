d6d0d46a3747d5a367e500155282237427fefb28
liqiang <liqiang@orvibo.com>
2025-06-26 17:40:49 +0800

    DependencyInjection

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/DependencyInjectionBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DependencyInjecttionBootcamp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by liqiang on 6/26/25.
+//
+
+import SwiftUI
+
+struct DependencyInjectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DependencyInjectionBootcamp()
+}
